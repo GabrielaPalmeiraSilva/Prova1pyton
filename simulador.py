d1bc60928c2d0395060b79fbf1fd2db2bfe5d2fb
GabrielaPalmeiraSilva <gabrielapalmeirasilva2510@gmail.com>
2026-03-31 13:07:55 -0300

    Add files via upload

---
+++ simulador.py
@@ -0,0 +1,9 @@
+#--O Game Over--#
+resposta = input("Deseja continuar jogando? (sim/não): ").lower()
+
+while resposta == "sim":
+    print("Você ainda está no jogo!")
+    resposta = input("Deseja continuar jogando?") (sim/não): ").lower()
+
+print("Game Over! ")
+#--Gabriela Palmeira--#
